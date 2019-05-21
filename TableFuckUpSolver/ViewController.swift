b9604bc48a215e96e69c28f8bf4144ff73676a4e
Christopher Stock <christopher.stock@mayflower.de>
2019-05-21 10:55:22 +0200

    Added empty iOS project.

---
+++ TableFuckUpSolver/ViewController.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+///
+/// The UI ViewController for the View Controller.
+///
+class ViewController : UIViewController
+{
+    ///
+    /// Do any additional setup after loading the view, typically from a nib.
+    ///
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+
+
+
+    }
+}
